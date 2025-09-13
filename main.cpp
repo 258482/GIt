9cf3fe62ee9d721f23016a8335c2502560c4caa4
Ql0ck1ch <andreylihosherstow1@gmail.com>
2025-09-13 17:08:23 +0200

    Zmiana metody Policz na a+b) * (a-b)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int Policz(int a , int b) {
-    return (a+b) * (a-b);
+    return (a+b)*(a-b) + 1 ;
 }
 
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszym branchu; 2*3=" << Policz(2,3) << std::endl;
+    std::cout << "W brancuchu 3 ;  (2+3)*(2-3)+1=" << Policz(2,3) << std::endl;
     return 0;
 }
